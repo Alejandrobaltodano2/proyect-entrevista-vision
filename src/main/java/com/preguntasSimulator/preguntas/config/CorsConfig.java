ee15e1c33de5a001b66991d5c9a5dafb03dd3dbd
janobaf <janobaf2@gmail.com>
2026-07-10 14:30:42 -0500

    corrigiendo tema de cors

--- src/main/java/com/preguntasSimulator/preguntas/config/CorsConfig.java
+++ src/main/java/com/preguntasSimulator/preguntas/config/CorsConfig.java
@@ -17,7 +17,11 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500")
+                        .allowedOriginPatterns(
+                                "http://127.0.0.1:*",
+                                "http://localhost:*",
+                                "https://*.devtunnels.ms"
+                        )
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
